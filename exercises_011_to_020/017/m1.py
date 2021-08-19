b4f4de9493c3ded0864bd7fbe04715348d367860
panyanyany <568397440@qq.com>
2021-08-19 10:30:09 +0800

    017

--- exercises_011_to_020/017/m1.py
+++ exercises_011_to_020/017/m1.py
@@ -5,5 +5,5 @@ def print2(*args, end='\n'):
     sys.stdout.write(' '.join(args) + end)
 
 
-print2('hello', 'world', end='')
+print2('hello', 'world', end='\n\n\n')
 print2('hello', 'world')
--- exercises_011_to_020/017/m3.py
+++ exercises_011_to_020/017/m3.py
@@ -3,7 +3,6 @@ import sys
 
 def print2(*args, sep=' ', end='\n', file=sys.stdout):
     file.write(sep.join(args) + end)
-    file.flush()
 
 
 print2('hello', 'world', file=open('out.txt', 'w+'))
